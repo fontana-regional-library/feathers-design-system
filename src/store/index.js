f078ffb446d448e9c041aea3ea5f8e4a6c8a60bc
awestlib <awest@fontanalib.org>
2019-02-05 13:34:40 -0500

    prepopulate ? for url query parameters

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,17 @@ import axios from "axios";
 
 Vue.use(Vuex);
 const urls = {
-authors: 'https://fontana.librarians.design/wp-json/wp/v2/users',
-callsToAction: 'https://fontana.librarians.design/wp-json/wp/v2/calls-to-action',
+authors: 'https://fontana.librarians.design/wp-json/wp/v2/users?',
+callsToAction: 'https://fontana.librarians.design/wp-json/wp/v2/calls-to-action?',
 collection: 'https://fontana.librarians.design/wp-json/wp/v2/collection?_embed',
-featuredCollections: 'https://fontana.librarians.design/wp-json/wp/v2/featured-collections',
-locations: 'https://fontana.librarians.design/wp-json/wp/v2/locations',
-pages: 'https://fontana.librarians.design/wp-json/wp/v2/pages',
+featuredCollections: 'https://fontana.librarians.design/wp-json/wp/v2/featured-collections?',
+locations: 'https://fontana.librarians.design/wp-json/wp/v2/locations?',
+pages: 'https://fontana.librarians.design/wp-json/wp/v2/pages?',
 posts: 'https://public-api.wordpress.com/rest/v1.1/sites/fontanalib.wordpress.com/posts/?number=10',
-articles: 'https://fontana.librarians.design/wp-json/wp/v2/posts',
-resources: 'https://fontana.librarians.design/wp-json/wp/v2/resources',
+articles: 'https://fontana.librarians.design/wp-json/wp/v2/posts?',
+resources: 'https://fontana.librarians.design/wp-json/wp/v2/resources?',
 services: 'https://fontana.librarians.design/wp-json/wp/v2/services?per_page=50',
-events: 'https://fontana.librarians.design/wp-json/wp/v2/events',
+events: 'https://fontana.librarians.design/wp-json/wp/v2/events?',
 };
 
 export default new Vuex.Store({
