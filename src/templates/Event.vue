e6508b6607d640baa040f7bc243aedc7ec09c31c
awestlib <awest@fontanalib.org>
2019-02-07 16:49:09 -0500

    loads the event object if it's not passed via the router

--- src/templates/Event.vue
+++ src/templates/Event.vue
@@ -1,26 +1,26 @@
 <template>
     <main class="background--white event" role="main">
 
-        <article v-if="eventObject">
+        <article v-if="event">
 
             <header class="col-11 col-md-10 col-lg-6 m-auto p-lg-4">
 
-                <heading class="event__title text--dark text--serif">{{ eventObject.title }}</heading>
+                <heading class="event__title text--dark text--serif">{{ event.title }}</heading>
 
                 <span class="event__time text--dark">
-                    {{eventObject.start_date | moment("dddd, MMMM Do YYYY h:mm a")}} &mdash;
-                    {{eventObject.end_date | moment("dddd, MMMM Do YYYY h:mm a")}}
+                    {{event.start_date | moment("dddd, MMMM Do YYYY h:mm a")}} &mdash;
+                    {{event.end_date | moment("dddd, MMMM Do YYYY h:mm a")}}
                 </span>
 
                 <div class="heading__separator"></div>
 
-                <p class="event__excerpt" v-if="eventObject.excerpt" v-html="eventObject.excerpt"></p>
+                <p class="event__excerpt" v-if="event.excerpt" v-html="event.excerpt"></p>
 
-                <add-to-calendar :title="eventObject.title"
-                                 :location="eventObject.venue.venue"
-                                 :start="new Date(`${eventObject.start_date}`)"
-                                 :end="new Date(`${eventObject.end_date}`)"
-                                 :details="eventObject.description"
+                <add-to-calendar :title="event.title"
+                                 :location="event.venue.venue"
+                                 :start="new Date(`${event.start_date}`)"
+                                 :end="new Date(`${event.end_date}`)"
+                                 :details="event.description"
                                  inline-template>
 
                     <div class="mb-3">
@@ -63,22 +63,22 @@
 
                 <div class="col-11 col-md-10 col-lg-6 m-auto pt-4 pb-4 p-lg-4">
 
-                    <img :src="eventObject.image.url" alt="">
+                    <img :src="event.image.url" alt="">
 
                     <heading class="text--dark" level="h2">About</heading>
 
-                    <div v-html="eventObject.description"></div>
+                    <div v-html="event.description"></div>
 
                     <heading class="text--dark" level="h3">When</heading>
-                    {{eventObject.start_date | moment("dddd, MMMM Do YYYY h:mm a")}}
-                    &mdash; {{eventObject.end_date | moment("dddd, MMMM Do YYYY h:mm a")}}
+                    {{event.start_date | moment("dddd, MMMM Do YYYY h:mm a")}}
+                    &mdash; {{event.end_date | moment("dddd, MMMM Do YYYY h:mm a")}}
 
                     <heading class="text--dark" level="h3">Where</heading>
 
-                    <router-link class="d-block text--dark text--underline" :to="`/location/${eventObject.venue.slug}`">{{ eventObject.venue.venue }}</router-link>
-                    <span class="d-block">{{ eventObject.venue.address }}</span>
-                    <span class="d-block">{{ eventObject.venue.city }}, {{ eventObject.venue.state }} {{ eventObject.venue.zip }}</span>
-                    <span class="d-block">{{ eventObject.venue.phone }}</span>
+                    <router-link class="d-block text--dark text--underline" :to="`/location/${event.venue.slug}`">{{ event.venue.venue }}</router-link>
+                    <span class="d-block">{{ event.venue.address }}</span>
+                    <span class="d-block">{{ event.venue.city }}, {{ event.venue.state }} {{ event.venue.zip }}</span>
+                    <span class="d-block">{{ event.venue.phone }}</span>
 
                 </div>
             </section>
@@ -95,6 +95,7 @@ import VueMoment from 'vue-moment';
 
 import CallToAction from '../patterns/CallToAction.vue';
 import Heading from '../elements/Heading.vue';
+import {urls} from "../variables.js";
 
 Vue.use(AddToCalendar);
 Vue.use(VueMoment);
@@ -106,13 +107,39 @@ export default {
     CallToAction,
     Heading,
   },
-
+  created(){
+    this.fetchData();
+  },
+  watch: {
+    // call again the method if the route changes
+    '$route': 'fetchData'
+  },
   computed: {
     author() {
-      return this.$store.getters.getAuthorById(Number(this.eventObject.author));
+      return this.$store.getters.getAuthorById(Number(this.event.author));
     },
   },
-
+  data(){
+    return{
+      event: this.event
+    };
+  },
+  methods: {
+    fetchData(){
+      if(!this.event){
+        this.event = this.$store.getters.getEventBySlug(this.$route.params.slug);
+      }
+      if(!this.event){
+        axios.get(urls.events + `slug=${this.$route.params.slug}`)
+        .then((response) =>{
+          this.event = response.data[0];
+        })
+        .catch( (error)=>{
+          console.log(error);
+        })
+      }
+    },
+  },
   props: {
     eventObject: {
       type: Object,
